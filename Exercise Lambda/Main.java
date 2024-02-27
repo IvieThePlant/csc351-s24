82eb8a25e5a5e364f3d659aeb6715b548139951c
IvieThePlant <ivietheplant@outlook.com>
2024-02-27 16:57:06 -0600

    working past the deadline

--- Exercise Lambda/Main.java
+++ Exercise Lambda/Main.java
@@ -7,9 +7,9 @@ public class Main {
     private static AlphaNumeric[] array = new AlphaNumeric[20];
 
     public static void main(String[] args) {
-    
-    	// Populate the data structures for testing
-    	fillStructures();
+
+        // Populate the data structures for testing
+        fillStructures();
 
         // Practice creating and applying functions
         functionPractice();
@@ -19,93 +19,113 @@ public class Main {
         queryWithFunctions();
     }
 
-    //______________________________________________________________________
-    //______________________________________________________________________
+    // ______________________________________________________________________
+    // ______________________________________________________________________
     public static void functionPractice() {
-    
-    	// Define each of these functions to extract either the alpha component or the numeric component. Your choice for Object.
 
-        Function<AlphaNumeric,String> fnS;
-        Function<AlphaNumeric,Integer> fnI;
-        Function<AlphaNumeric,Object> fnO;
-        
+        // Define each of these functions to extract either the alpha component or the
+        // numeric component. Your choice for Object.
+
+        Function<AlphaNumeric, String> fnS = (alNum) -> alNum.alpha();
+        Function<AlphaNumeric, Integer> fnI = (alNum) -> alNum.number();
+        Function<AlphaNumeric, Object> fnO = (alNum) -> alNum.number();
+
         // Pass your functions to these tests to see if they are working.
-        //testAlpha(...);
-        //testNumer(...);
-        //testObject(...);
+        testAlpha(fnS);
+        testNumber(fnI);
+        testObject(fnO);
     }
 
-    public static void testAlpha(Function<AlphaNumeric,String> fn) {
+    public static void testAlpha(Function<AlphaNumeric, String> fn) {
         // Print the results of applying the function to each element in array.
         System.out.println("\n-- Function testAlpha");
+        for (AlphaNumeric a : array) {
+            System.out.print(fn.apply(a) + " ");
+        }
+        System.out.println();
     }
 
-    public static void testNumber(Function<AlphaNumeric,Integer> fn) {
+    public static void testNumber(Function<AlphaNumeric, Integer> fn) {
         // Print the results of applying the function to each element in array.
         System.out.println("\n-- Function testNumber");
-    }
-    
-    public static void testObject(Function<AlphaNumeric,Object> fn) {
-        // Print the results of applying the function to each element in array.
-        System.out.println("\n-- Function testObject");
+        for (AlphaNumeric a : array) {
+            System.out.print(fn.apply(a) + " ");
+        }
+        System.out.println();
     }
 
-    //______________________________________________________________________
-    //______________________________________________________________________
+    public static void testObject(Function<AlphaNumeric, Object> fn) {
+        // Print the results of applying the function to each element in array.
+        System.out.println("\n-- Function testObject");
+        for (AlphaNumeric a : array) {
+            System.out.print(fn.apply(a) + " ");
+        }
+        System.out.println();
+    }
+
+    // ______________________________________________________________________
+    // ______________________________________________________________________
     public static void queryWithFunctions() {
 
         // Create a Function that returns a Boolean for matching the letter x
-        
-        // query the List items to find all "x" AlphaNumerics, save to ArrayList
-        
-        // print found elements
-        System.out.println("\n-- Results of query for x");  
+        Function<AlphaNumeric, Boolean> isX = (a) -> a.alpha().equals("x");
 
-        
-        // Create a Function that returns a Boolean for matching all numbers>=2
-        
-        // query the List items to find all that are >= 2, save to ArrayList
-        
+        // query the List items to find all "x" AlphaNumerics, save to ArrayList
+        ArrayList<AlphaNumeric> results = List.query(isX);
+
         // print found elements
-        System.out.println("\n-- Results of query for >=2");  
+        System.out.println("\n-- Results of query for x");
+        for(AlphaNumeric a : results) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
+
+        // Create a Function that returns a Boolean for matching all numbers>=2
+        Function<>
+
+
+        // query the List items to find all that are >= 2, save to ArrayList
+
+        // print found elements
+        System.out.println("\n-- Results of query for >=2");
 
     }
-    
-    //________________________________________________________________________
+
+    // ________________________________________________________________________
     public static void fillStructures() {
-            // Fill a List and an array to practice with
-        for (int i=0; i<4; i++) {
-        	AlphaNumeric an = new AlphaNumeric("a",i);
-        	items.add(an);	
-        	array[i] = an;
+        // Fill a List and an array to practice with
+        for (int i = 0; i < 4; i++) {
+            AlphaNumeric an = new AlphaNumeric("a", i);
+            items.add(an);
+            array[i] = an;
         }
-        for (int i=0; i<4; i++) {
-        	AlphaNumeric an = new AlphaNumeric("g",i);
-        	items.add(an);	
-        	array[i+4] = an;
+        for (int i = 0; i < 4; i++) {
+            AlphaNumeric an = new AlphaNumeric("g", i);
+            items.add(an);
+            array[i + 4] = an;
         }
-        for (int i=0; i<4; i++) {
-        	AlphaNumeric an = new AlphaNumeric("m",i);
-        	items.add(an);	
-        	array[i+8] = an;
+        for (int i = 0; i < 4; i++) {
+            AlphaNumeric an = new AlphaNumeric("m", i);
+            items.add(an);
+            array[i + 8] = an;
         }
-        for (int i=0; i<4; i++) {
-        	AlphaNumeric an = new AlphaNumeric("s",i);
-        	items.add(an);	
-        	array[i+12] = an;	
+        for (int i = 0; i < 4; i++) {
+            AlphaNumeric an = new AlphaNumeric("s", i);
+            items.add(an);
+            array[i + 12] = an;
         }
-        for (int i=0; i<4; i++) {
-        	AlphaNumeric an = new AlphaNumeric("x",i);
-        	items.add(an);	
-        	array[i+16] = an;
+        for (int i = 0; i < 4; i++) {
+            AlphaNumeric an = new AlphaNumeric("x", i);
+            items.add(an);
+            array[i + 16] = an;
         }
         System.out.println("\nThe List");
-		System.out.println(items);
-		
-		System.out.println("\nThe array");
-		for (AlphaNumeric an : array) {
-			System.out.print(an);
-		}
-		System.out.println();
-	}
+        System.out.println(items);
+
+        System.out.println("\nThe array");
+        for (AlphaNumeric an : array) {
+            System.out.print(an);
+        }
+        System.out.println();
+    }
 }
--- Sorting Analysis Project/Merge.java
+++ Sorting Analysis Project/Merge.java
@@ -9,34 +9,85 @@ public class Merge<T> implements Sorter<T> {
     /** Counter of compare operations */
     long count = 0;
 
-
-    /** Constructor for Merge Sort to set comparator
-    *
-    * @param order Comparator to establish ordering of array elements.
-    */
+    /**
+     * Constructor for Merge Sort to set comparator
+     *
+     * @param order Comparator to establish ordering of array elements.
+     */
     public Merge(Comparator<T> order) {
         orderBy = order;
     }
 
-    /** Sorts specified array using Merge Sort
-    *
-    * @param array Array to be sorted.
-    */
+    /**
+     * Sorts specified array using Merge Sort
+     *
+     * @param array Array to be sorted.
+     */
     @Override
     public void sort(T[] array) {
 
         // Reset count back to 0
         count = 0;
 
-        // _________________________________________________________________
-        // TODO ___________ COMPLETE MERGE SORT BELOW __________________
-        // ________________   IMPORTANT TO COMMENT YOUR CODE _______________
-
-
-
-        // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        reSort(array, 0, array.length);
 
     } // end sort(T[])
+
+    // Recurivly splits array in half
+    public void reSort(T[] A, int p, int r) {
+        if (p < r) {
+            // Finds where to bisect subarray from p to r
+            int q = (p + r) / 2;
+
+            reSort(A, p, q);
+            reSort(A, q + 1, r);
+            squish(A, p, q, r);
+        }
+        count++;
+    }
+
+    // Sorts two half lists back together.
+    public void squish(T[] A, int p, int q, int r) {
+        // Find the lengths of each half
+        int nOne = q - p + 1;
+        int nTwo = r - q;
+
+        // Create empty arrays to store each half
+        @SuppressWarnings("Unchecked")
+        T[] Left = (T[]) new Object[nOne];
+        @SuppressWarnings("Unchecked")
+        T[] Right = (T[]) new Object[nTwo];
+
+        // Copy halves into subarrays
+        for (int i = 0; i < nOne; i++) {
+            Left[i] = A[p + i];
+        }
+        for (int j = 0; j < nTwo; j++) {
+            Right[j] = A[q + j + 1];
+        }
+
+        // Initialize two pointers starting at each half
+        int i = 0;
+        int j = 0;
+
+        // Merge two halves at each index
+        for (int k = p; k <= r; k++) {
+            if (i == Left.length) {
+                System.arraycopy(Right, j, A, k, Right.length - j);
+                break;
+            } else if (j == Right.length) {
+                System.arraycopy(Left, i, A, k, Left.length - i);
+                break;
+            } else if (0 > orderBy.compare(Left[i], Right[j])) { // Take the next lowest value from either half
+                A[k] = Left[i];
+                i++;
+            } else {
+                A[k] = Right[j];
+                j++;
+            }
+            count++;
+        }
+    }
 
     @Override
     public void setComparator(Comparator<T> order) {
